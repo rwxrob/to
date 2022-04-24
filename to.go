7ed09575c05da90098a7ce1a39480b45ff190c09
rwxrob <rob@rwx.gg>
2022-04-24 10:40:12 -0400

    Add StopWatch

--- to.go
+++ to.go
@@ -11,10 +11,12 @@ package to
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"reflect"
 	"regexp"
 	"runtime"
 	"strings"
+	"time"
 	"unicode"
 
 	"github.com/rwxrob/fn/maps"
@@ -234,6 +236,42 @@ func MergedMaps[K comparable, V any](maps ...map[K]V) map[K]V {
 		}
 	}
 	return combined
+}
+
+// StopWatch converts a duration into a string that one would expect to
+// see on a stopwatch.
+func StopWatch(dur time.Duration) string {
+	var out string
+
+	sec := dur.Seconds()
+
+	if sec >= 3600 {
+		hours := sec / 3600
+		sec = math.Mod(sec, 3600)
+		out += fmt.Sprintf("%v:", int(hours))
+	}
+
+	if sec >= 60 {
+		var form string
+		mins := sec / 60
+		sec = math.Mod(sec, 60)
+		if len(out) == 0 {
+			form = `%v:`
+		} else {
+			form = `%02v:`
+		}
+		out += fmt.Sprintf(form, int(mins))
+	}
+
+	var form string
+	if len(out) == 0 {
+		form = `%02v`
+	} else {
+		form = `%02v`
+	}
+	out += fmt.Sprintf(form, int(sec))
+
+	return out
 }
 
 // EscReturns changes any actual carriage returns or line returns into
